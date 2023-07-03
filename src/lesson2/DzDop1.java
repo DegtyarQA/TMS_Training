c652219afc6c5cd3c1c11365897dc5edc8642633
vdegtjar <v.degtjar@cft.ru>
2023-07-03 22:24:52 +0300

    #12 done

---
+++ src/lesson2/DzDop1.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class DzDop1 {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " " + b +" ");
+        for (int i = 2; i < 11; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
