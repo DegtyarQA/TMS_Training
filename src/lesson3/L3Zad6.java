c25a14d01a3018cc1781c23aa9784283f00af525
vdegtjar <v.degtjar@cft.ru>
2023-07-11 09:03:46 +0300

    done z7

---
+++ src/lesson3/L3Zad6.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+import java.util.Arrays;
+
+public class L3Zad6 {
+    public static void main(String[] args) {
+        String str[] = new String[]{"Vsevolod", "Ivan", "Ihar", "Valery", "Kate", "John", "Elvira", "Max"};
+        Arrays.sort(str);
+        System.out.print(Arrays.toString(str));
+    }
+}
