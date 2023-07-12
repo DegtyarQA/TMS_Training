be21d81c504e8ec3141bdb20334aacf523ea4ed4
vdegtjar <v.degtjar@cft.ru>
2023-07-12 07:36:08 +0300

    z3 fixed after review

--- src/lesson3/L3Zad1.java
+++ src/lesson3/L3Zad1.java
@@ -6,17 +6,15 @@ public class L3Zad1 {
     public static void main(String[] args) {
         int[] array1 = new int[]{1, 2, 6, 8, 9, 11, 25, 23, 99, 34};
         Scanner sc1 = new Scanner(System.in);
-        int b = 0;
         int a;
         System.out.print("Enter number: ");
         a = sc1.nextInt();
         for (int i = 0; i < array1.length; i++) {
             if (array1[i] == a) {
-                System.out.print("number in array1!!!" + b);
+                System.out.print("number in array1!!!");
                 break;
             }
-            b++;
         }
-        System.out.print("number is NOT in array1!!!" + b);
+        System.out.print("number is NOT in array1!!!");
     }
 }
