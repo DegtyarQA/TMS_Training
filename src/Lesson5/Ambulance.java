a4e3d2d3a9a2e9bd3aae0dd534e81adf466a8ea4
vdegtjar <v.degtjar@cft.ru>
2023-07-19 09:41:54 +0300

    fixed afrer review

--- src/Lesson5/Ambulance.java
+++ src/Lesson5/Ambulance.java
@@ -3,9 +3,9 @@ package Lesson5;
 
 public class Ambulance {
     public static void main(String[] args) {
-        Surgeon surgeon = new Surgeon("Victor", "Surgeon", true, "abdominal");
-        Therapist therapist = new Therapist("Alex", "Therapist", "hight");
-        Dantist dantist = new Dantist("Nick", "Dantist", true, 55);
+        Doctor surgeon = new Surgeon("Victor", "Surgeon", true, "abdominal");
+        Doctor therapist = new Therapist("Alex", "Therapist", "hight");
+        Doctor dantist = new Dantist("Nick", "Dantist", true, 55);
         Pacient pacient = new Pacient("Olga", 44, "F");
         Plan plan = new Plan(6);
         plan.asigneeDctor();
